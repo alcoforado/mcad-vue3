85264fb465be88d2c9894aa91309216b58f85dac
marcosalcoforado <marcosalcoforado@gmail.com>
2021-07-31 23:37:03 -0400

    AddingMaterial Icons

---
+++ src/components/button-icon.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+<style lang="scss">
+    .app-bar {
+        width:100%;
+        height: 45px;
+        position: fixed;
+        background-color: $primary;
+        color: $primary-font-color;
+        vertical-align: center;
+    }
+</style>
