cf8cbb052784cb25658d9fb304866b5b5da5b648
Marcos Mendes <MMendes@ConnectWise.com>
2021-08-21 00:13:57 -0400

    Adding ripple

--- src/components/button-icon.vue
+++ src/components/button-icon.vue
@@ -1,10 +1,21 @@
 <template>
   
-        <span class="button-icon material-icons">{{icon}}</span>
-    
+        <span v-ripple class="button-icon material-icons">{{icon}}</span>
+        
 </template>
 <style lang="scss">
-
+.button-icon {
+  border-radius:10000px;
+  padding: 6px;
+  cursor: pointer;
+  font-size:24px;
+  user-select: none;
+  transition: background-color  0.6s;
+  &:hover {
+      background-color: $font-color-contrast
+  }
+}
+    
 </style>
 
 <script lang="ts">
